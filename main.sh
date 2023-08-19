3f61a21f89689c5a0e44b8a607bed1b639923db1
ignasa007 <jasraj001@e.ntu.edu.sg>
2023-08-19 16:45:06 +0800

    slight modification to driver

--- main.sh
+++ main.sh
@@ -1,15 +1,16 @@
-model='albert-base-v2'
 DEVICE_INDEX=0
 
-for run in {1..4}; do
-    for dataset in {aaai-constraint-covid,aaai-constraint-covid-appended}; do
-        for ADD_NEW_TOKENS in {False,True}; do
-            python3 -B main.py \
-                --dataset ${dataset} \
-                --model ${model} \
-                ADD_NEW_TOKENS ${ADD_NEW_TOKENS} \
-                DATA.SAVE_EVERY None \
-                DEVICE_INDEX ${DEVICE_INDEX};
+for run in {1..5}; do
+    for dataset in {aaai-constraint-covid,aaai-constraint-covid-appended,}; do
+        for model in {albert-base-v2,distilbert-base-uncased,}; do
+            for ADD_NEW_TOKENS in {False,True}; do
+                python3 -B main.py \
+                    --dataset ${dataset} \
+                    --model ${model} \
+                    ADD_NEW_TOKENS ${ADD_NEW_TOKENS} \
+                    DATA.SAVE_EVERY None \
+                    DEVICE_INDEX ${DEVICE_INDEX};
+            done
         done
     done
 done
