b7a49baa7d1dd852bbb3c45abf244d267d1440ee
ignasa007 <jasraj001@e.ntu.edu.sg>
2023-08-21 15:11:53 +0800

    more results

--- main.sh
+++ main.sh
@@ -1,16 +1,13 @@
 DEVICE_INDEX=1
+dataset='covid-misinformation'
 
-for run in {1..1}; do
-    for dataset in {covid-misinformation,}; do
-        for model in {albert-base-v2,distilbert-base-uncased,}; do
-            for ADD_NEW_TOKENS in {True,}; do
-                python3 -B main.py \
-                    --dataset ${dataset} \
-                    --model ${model} \
-                    ADD_NEW_TOKENS ${ADD_NEW_TOKENS} \
-                    DATA.SAVE_EVERY None \
-                    DEVICE_INDEX ${DEVICE_INDEX};
-            done
-        done
+for run in {1..4}; do
+    for model in {covid-twitter-bert-v2,twitter-roberta-base-sentiment-latest,albert-base-v2,distilbert-base-uncased}; do
+        python3 -B main.py \
+            --dataset ${dataset} \
+            --model ${model} \
+            ADD_NEW_TOKENS True \
+            DATA.SAVE_EVERY None \
+            DEVICE_INDEX ${DEVICE_INDEX};
     done
 done
