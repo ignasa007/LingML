a7716308e21790e78236f37d614ffc2018829b14
ignasa007 <jasraj001@e.ntu.edu.sg>
2023-08-24 02:29:32 +0800

    covidmis20 results

--- main.sh
+++ main.sh
@@ -7,11 +7,12 @@ dataset='covid-misinformation'
 # distilbert-base-uncased
 
 for run in {1..1}; do
-    for model in {covid-twitter-bert-v2,albert-base-v2}; do
+    for model in {albert-base-v2,covid-twitter-bert-v2}; do
         python3 -B main.py \
             --dataset ${dataset} \
             --model ${model} \
             ADD_NEW_TOKENS True \
+            # LR 0.00002 \
             DATA.SAVE_EVERY None \
             DEVICE_INDEX ${DEVICE_INDEX};
     done
