653ad485e928ce7690d1f232759dce0e2f686255
ignasa007 <jasraj001@e.ntu.edu.sg>
2023-09-20 05:34:00 +0800

    more results

--- main.sh
+++ main.sh
@@ -1,12 +1,8 @@
 for dataset in {aaai-constraint-covid-appended,aaai-constraint-covid}; do
-    for run in {1..5}; do
-        for ADD_NEW_TOKENS in {False,True,}; do
-            python3 -B main.py \
-                --dataset ${dataset} \
-                --model longformer-base-4096 \
-                ADD_NEW_TOKENS ${ADD_NEW_TOKENS} \
-                DATA.SAVE_EVERY None \
-                DEVICE_INDEX 1;
-        done
-    done
+    python3 -B main.py \
+        --dataset ${dataset} \
+        --model xlm-mlm-en-2048 \
+        ADD_NEW_TOKENS False \
+        DATA.SAVE_EVERY None \
+        DEVICE_INDEX 1;
 done
